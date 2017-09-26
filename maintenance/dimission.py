bddeb7c060835a825baefe249b25374cb0da09ee
lixiaoguo <lixiaoguo@126.com>
2017-09-26 09:24:33 +0800

    commit

--- maintenance/dimission.py
+++ maintenance/dimission.py
@@ -16,11 +16,10 @@ def get_dimission_info():
             logging.info('empl_id={},set_id={}'.format(empl_id,set_id))
             try:
                 sb.session.execute("""
-                                   update user set status='I' where code='{}' and set_id='{}';
-                                   update user_acms_rela set state='I'
+                                   update user set status='I',status_date=now() where code='{}' and set_id='{}';
+                                   update user_acms_rela set state='I',state_date=now()
                                    where user_id in (select user_id from user where code='{}')
                                    """.format(empl_id,set_id,empl_id))
-                sb.session.commit()
                 xai=sb.session.execute("""
                             select acms,sr.anum from user u,user_acms_rela uar,acms a,subs_rela sr
                             where u.user_id = uar.user_id
@@ -36,13 +35,14 @@ def get_dimission_info():
                     prtms=j[1]
 
                     if acms and prtms:
-                        r=f_unbind('wywiwj',prtms,acms,"+ZOLLJ/EFIQEVIWM")
+                        r=f_unbind('wywawj',prtms,acms,"+ZOLLJ/EFIQEVIWM")
                         logging.info(r)
 
-                    sb.session.execute("""
-                                       update acms a set a.status='I' where acms='{}';
-                                       update subs_rela  set state='U',state_date=now() where anum='{}' and xnum='{}' and state='B';
-                                       """.format(acms,acms,prtms,empl_id,set_id,empl_id,set_id))
+                        if r['result']=='200':
+                            sb.session.execute("""
+                                               update acms a set a.status='I',status=now()  where acms='{}';
+                                               update subs_rela  set state='U',state_date=now() where anum='{}' and xnum='{}' and state='B';
+                                               """.format(acms,acms,prtms,empl_id,set_id,empl_id,set_id))
                 sb.session.execute("""
                                    insert into emp_dismission_his
                                    select * from emp_dismission where empl_id='{}' and set_id='{}';
@@ -50,7 +50,7 @@ def get_dimission_info():
                                    """.format(empl_id,set_id,empl_id,set_id))
                 sb.session.commit()
             except Exception as ex:
-                print(ex)
+                logging.info(ex)
             cnt+=1
             logging.info(cnt)
             logging.info('update dismission success!')
@@ -58,33 +58,52 @@ def get_dimission_info():
         logging.info(ex)
     finally:
         sb.session.close()
+
 def get_personal_ad():
-    last_batchnum=rd.get('emp_personal')
     try:
         sb.session.execute("""
-            drop table if exists temp_empl;
-            create table temp_empl
-            select e.empl_id,ep.name_format,ep.sex,ep.set_id,e.c_mobile
-            from erp e ,
-            (select name_format,sex,set_id,empl_id
-            from emp_personal ep0 where batch_num>{} and
-            not exists (select 1 from user where code=ep0.empl_id)) ep
-            where e.empl_id=ep.empl_id ;
-            insert into user(org_id,code,name,sex,duty,status,status_date,tel,set_id)
-            select ed.dept_id,ep.empl_id,ep.name_format,ep.sex ,drr.name,'V',current_date(),ep.c_mobile,ep.set_id
-            from
-            temp_empl ep
-            inner join
-            (select ed2.empl_id,ed2.dept_id,ed2.job_code,ed2.hr_status,ed2.setid_job_code,ed2.c_quarters_id from
-            (select empl_id,max(transfer_date_time) transfer_date_time
-            from emp_duty group by empl_id) ed1
-            inner join emp_duty ed2
-            on ed1.empl_id=ed2.empl_id and ed1.transfer_date_time=ed2.transfer_date_time  ) ed
-            on ep.empl_id=ed.empl_id and ep.set_id=ed.setid_job_code
-            inner join duty_role_rela drr
-            on drr.c_quarters_id=ed.c_quarters_id and drr.set_id=ed.setid_job_code
-            where ed.hr_status='A' ;
-             """.format(int(last_batchnum)))
+                drop table if exists temp_empl;
+                create table temp_empl
+                select e.empl_id,ep1.name_format,ep1.sex,ep1.set_id,
+                case when ep1.set_id='10002' then ep1.c_mobile else e.c_mobile end 'c_mobile'
+                from erp e ,
+                (select name_format,sex,set_id,empl_id,c_mobile,transfer_date_time
+                from emp_personal) ep1 ,
+                (select empl_id,max(transfer_date_time) transfer_date_time
+                from emp_personal group by empl_id) ep2
+                where e.empl_id=ep1.empl_id
+                and ep1.empl_id=ep2.empl_id
+                and ep1.transfer_date_time=ep2.transfer_date_time;
+
+                drop table if exists temp_user;
+                create table temp_user
+                select ed.dept_id,ep.empl_id,ep.name_format,ep.sex ,vdi.descr,'V' status,current_date() create_date,ep.c_mobile,ep.set_id
+                from
+                ( select distinct * from temp_empl) ep
+                inner join
+                (select ed2.empl_id,ed2.dept_id,ed2.hr_status,ed2.setid_job_code set_id,ed2.job_code from
+                (select empl_id,max(transfer_date_time) transfer_date_time
+                from emp_duty where hr_status='A' group by empl_id) ed1
+                inner join emp_duty ed2
+                on ed1.empl_id=ed2.empl_id and ed1.transfer_date_time=ed2.transfer_date_time  ) ed
+                on ep.empl_id=ed.empl_id and ep.set_id=ed.set_id
+                inner join v_duty_info vdi
+                on vdi.job_code=ed.job_code and vdi.set_id=ed.set_id
+                where ed.hr_status='A';
+
+                update user u,temp_user t
+                set u.duty=t.descr
+                where u.code=t.empl_id;
+
+                insert into user(org_id,code,name,sex,duty,status,status_date,tel,set_id)
+                select * from temp_user tu where not exists (select 1 from user u where u.code=tu.empl_id);
+
+                delete from user_role;
+                insert into user_role
+                select u.user_id,role_id,'V',now() from
+                (select user_id,duty from user where duty in ('经纪人','店经理','区经理','区总监','管理员')) u
+                inner join role on  role.role_name= u.duty;
+             """)
         sb.session.commit()
         batch_num_i=sb.session.execute("select max(batch_num) from emp_personal").fetchone()
         batch_num_max=batch_num_i[0]
@@ -99,46 +118,50 @@ def get_personal_ad():
 def get_org_al():
     try:
         sb.session.execute("""
-            drop table temp_dept;
+            drop table  if exists temp_dept;
+
             create table temp_dept
             as
                 select
                  dept.tree_node,
                  CAST(dept.parent_node_name AS SIGNED integer) as parent_node_name,
-               descr,
+                 descr,
                  status,
                  status_date,
                  operator,
                  set_id,
                  transfer_date_time
-                from
-                (select CAST(dt.tree_node AS SIGNED integer) as tree_node ,
-                        case when dt.parent_node_name = 'null' then '0' else dt.parent_node_name end as parent_node_name,
+            FROM
+            (select CAST(dt1.tree_node AS SIGNED integer) as tree_node ,
+                        case when dt1.parent_node_name = 'null' then '0' else dt1.parent_node_name end as parent_node_name,
                         di.descr,
                         'V' status,
-                        now() status_date,1 operator,dt.set_id AS set_id ,dt.transfer_date_time
-                 from
-                 (select dt1.* from dept_tree dt1
-                 inner join (select set_id,tree_node,max(transfer_date_time) transfer_date_time
-                             from dept_tree
-                             group by set_id,tree_node) dt2
-               on dt1.tree_node=dt2.tree_node
-                 and dt1.set_id = dt2.set_id
-                 and dt1.transfer_date_time=dt2.transfer_date_time) dt
-                inner join
-                  (select di2.* from
-                  (select dept_id,set_id,max(transfer_date_time) transfer_date_time
-                from dept_info
-                where eff_status='A'
-                group by dept_id,set_id) di1
-                inner join
-                dept_info di2
-                on di1.dept_id=di2.dept_id
-                and di1.set_id=di2.set_id
-                and di1.transfer_date_time=di2.transfer_date_time
-                ) di
-                  on di.dept_id=dt.tree_node and dt.set_id=di.set_id) dept
-                  where dept.descr is not null;
+                        now() status_date,1 operator,dt1.set_id AS set_id ,dt1.transfer_date_time
+            from
+            (select set_id,tree_node,max(transfer_date_time) transfer_date_time ,max(effdt) effdt,parent_node_name from dept_tree
+            group by set_id,tree_node,parent_node_name) dt1
+            inner join (select set_id,tree_node,max(transfer_date_time) transfer_date_time,max(effdt) effdt
+            from dept_tree
+            group by set_id,tree_node) dt2
+            on dt1.tree_node=dt2.tree_node
+            and dt1.set_id = dt2.set_id
+            and dt1.transfer_date_time=dt2.transfer_date_time
+            and dt1.effdt = dt2.effdt
+            inner join
+              (select di2.* from
+              (select dept_id,set_id,max(transfer_date_time) transfer_date_time
+            from dept_info
+            where eff_status='A'
+            group by dept_id,set_id) di1
+            inner join
+            dept_info di2
+            on di1.dept_id=di2.dept_id
+            and di1.set_id=di2.set_id
+            and di1.transfer_date_time=di2.transfer_date_time
+            ) di
+              on di.dept_id=dt1.tree_node and dt1.set_id=di.set_id) dept
+            where dept.descr is not null;
+
             delete from org;
             insert into org (org_id,parent_org_id,org_name,status,status_date,operator,set_id)
             select  t2.tree_node,t2.parent_node_name,t2.descr,t2.status,t2.status_date,t2.operator,t2.set_id
@@ -146,6 +169,7 @@ def get_org_al():
             (select tree_node,parent_node_name,set_id,max(transfer_date_time) transfer_date_time
             from temp_dept
             group by tree_node,parent_node_name,set_id) t1
+
             inner join temp_dept t2
             on t1.tree_node=t2.tree_node
             and t1.parent_node_name=t2.parent_node_name
---
+++ maintenance/perday.py
@@ -0,0 +1,7 @@
+from maintenance import dimission
+from maintenance.dimission import get_personal_ad
+from maintenance.dimission import get_org_al
+if __name__ == '__main__':
+    get_personal_ad()
+    get_org_al()
+
